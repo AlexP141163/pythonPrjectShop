f1ab77466c118b3ab2c425190e855d3fb801d6c1
AlexP141163 <pilyugin.aa@gmail.com>
2024-07-29 18:22:34 +0300

    The main app ready pages index and about

--- app/main/views.py
+++ app/main/views.py
@@ -6,10 +6,18 @@ def index(request):
     context = {
         'title': 'Home - Главная',
         'content':"Магазин цветов FLOWERS",
-
     }
 
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse("About page")
+    context = {
+        'title': 'Home - О нас',
+        'content': "О нас",
+        'text_on_page': "В нашем магазине всегда самые свежие и красивые цветы. "
+                        "Вы всегда можете подобрать в нашем магазине любые цветы, букеты, корзины цветов"
+                        " на любой вкус. Мы очень стараемся, что бы удовлетворить Ваши желания. "
+                        "Приходите в наш магазин и Вы останетесь очень довольны."
+    }
+
+    return render(request, 'main/about.html', context)

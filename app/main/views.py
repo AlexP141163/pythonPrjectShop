3b5bd4d7e9849d7228253e21a4e7eb3486cd3430
AlexP141163 <pilyugin.aa@gmail.com>
2024-07-29 17:16:02 +0300

    add intermediate_1

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'Home - Главная',
+        'content':"Магазин цветов FLOWERS",
+
+    }
+
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    return HttpResponse("About page")

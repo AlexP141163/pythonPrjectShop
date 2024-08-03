2fb967a8b9024fdd06fdc7fa2bfd1e034884f2f7
AlexP141163 <pilyugin.aa@gmail.com>
2024-08-03 17:36:21 +0300

    The product filter on the site page and urlencode

--- app/goods/templatetags/goods_tags.py
+++ app/goods/templatetags/goods_tags.py
@@ -1,6 +1,6 @@
 # В этом файле будут храниться пользовательские шаблонные теги:
 from django import template
-
+from django.utils.http import urlencode
 from goods.models import Categories
 
 
@@ -8,3 +8,9 @@ register = template.Library() # Регистрация шаблонного те
 @register.simple_tag()
 def tag_categories(): # Фукция которая возвращает "Категории" на странице, по этому имени в шаблоне вернется 'Categories.objects.all()'
     return Categories.objects.all()
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(**kwargs)
+    return urlencode(query)

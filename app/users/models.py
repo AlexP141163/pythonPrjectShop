b1817b4275de1f70f799510c3149de8ca36bb721
AlexP141163 <pilyugin.aa@gmail.com>
2024-08-06 00:15:22 +0300

    Login form and controller are ready

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='Аватар')
+
+    class Meta:
+        db_table = 'user' # Имя таблицы в базе данных в единственном !!! числе:
+        verbose_name = 'Пользователя' # Имя будет отображаться в admin - панеле на русском языке в едиственном числе:
+        verbose_name_plural = 'Пользователи'  # Имя будет отображаться в admin-панеле на русском языке во множественном числе:
+
+    def __str__(self):
+        return self.username # Возвращает правильное название созданной новой группы имени:

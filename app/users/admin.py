aa36b074be7332e865f9055863dcc9409ae159ef
AlexP141163 <pilyugin.aa@gmail.com>
2024-08-12 17:33:08 +0300

    Create order is ready

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import User
 from carts.admin import CartTabAdmin
+from orders.admin import OrderTabulareAdmin
+
+from .models import User
+
+
 
 # admin.site.register(User)
 
@@ -9,4 +13,4 @@ class CategoriesAdmin(admin.ModelAdmin):
     list_display = ["username", "first_name", "last_name", "email",]
     search_fields = ["username", "first_name", "last_name", "email",]
 
-    inlines = [CartTabAdmin,]
+    inlines = [CartTabAdmin, OrderTabulareAdmin]

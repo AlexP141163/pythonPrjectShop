2b5e633e7e5014e30cfa637b0f992463632b4b14
AlexP141163 <pilyugin.aa@gmail.com>
2024-08-16 12:30:40 +0300

    Replacing functions with classes in the main, goods, and users modules.

---
+++ app/carts/views_bot.py
@@ -0,0 +1,122 @@
+import telegram
+from django.conf import settings
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+from django.urls import reverse
+from carts.models import Cart
+from carts.utils import get_user_carts
+from goods.models import Products
+
+# Инициализация бота с токеном
+bot = telegram.Bot(token=settings.TELEGRAM_BOT_TOKEN)
+
+def cart_add(request):
+    product_id = request.POST.get("product_id")
+    product = Products.objects.get(id=product_id)
+
+    if request.user.is_authenticated:
+        carts = Cart.objects.filter(user=request.user, product=product)
+
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        carts = Cart.objects.filter(session_key=request.session.session_key, product=product)
+
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(session_key=request.session.session_key, product=product, quantity=1)
+
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request
+    )
+
+    response_data = {
+        "message": "Товар добавлен в корзину",
+        "cart_items_html": cart_items_html,
+    }
+
+    return JsonResponse(response_data)
+
+def cart_change(request):
+    cart_id = request.POST.get("cart_id")
+    quantity = request.POST.get("quantity")
+
+    cart = Cart.objects.get(id=cart_id)
+
+    cart.quantity = quantity
+    cart.save()
+    updated_quantity = cart.quantity
+
+    user_cart = get_user_carts(request)
+
+    context = {"carts": user_cart}
+
+    referer = request.META.get('HTTP_REFERER')
+    if reverse('orders:create_order') in referer:
+        context["orders"] = True
+
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", context, request=request
+    )
+
+    response_data = {
+        "message": "Количество изменено",
+        "cart_items_html": cart_items_html,
+        "quantity": updated_quantity,
+    }
+
+    return JsonResponse(response_data)
+
+def cart_remove(request):
+    cart_id = request.POST.get("cart_id")
+    cart = Cart.objects.get(id=cart_id)
+    quantity = cart.quantity
+    cart.delete()
+
+    user_cart = get_user_carts(request)
+
+    context = {"carts": user_cart}
+
+    referer = request.META.get('HTTP_REFERER')
+    if reverse('orders:create_order') in referer:
+        context["orders"] = True
+
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", context, request=request
+    )
+
+    response_data = {
+        "message": "Товар удален",
+        "cart_items_html": cart_items_html,
+        "quantity_deleted": quantity,
+    }
+    return JsonResponse(response_data)
+
+def create_order(request):
+    user_cart = get_user_carts(request)
+    total_price = user_cart.total_price
+    items = "\n".join([f"{item.product.name} - {item.quantity} шт." for item in user_cart])
+
+    # Формирование сообщения
+    message = f"Новый заказ!\n\nТовары:\n{items}\n\nОбщая сумма: {total_price} руб."
+
+    # Отправка сообщения в Telegram
+    bot.send_message(chat_id=settings.TELEGRAM_CHAT_ID, text=message)
+
+    # Логика сохранения заказа и очистки корзины
+    # ...
+
+    response_data = {
+        "message": "Заказ успешно оформлен",
+    }
+    return JsonResponse(response_data)
--- app/main/views.py
+++ app/main/views.py
@@ -1,26 +1,51 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from goods.models import Categories
 
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
 
-def index(request):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - Главная'
+        context['content'] = "Магазин цветов FLOWERS"
+        return context
 
-    context = {
-        'title': 'Home - Главная',
-        'content':"Магазин цветов FLOWERS",
-    }
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
 
-    return render(request, 'main/index.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - О нас'
+        context['content'] = "О нас"
+        context['text_on_page'] = ("В нашем магазине всегда самые свежие и красивые цветы. "
+                                   "Вы всегда можете подобрать в нашем магазине любые цветы, "
+                                   "букеты, корзины цветов на любой вкус. Мы очень стараемся, "
+                                   "что бы удовлетворить Ваши желания. Приходите в наш магазин и "
+                                   "Вы останетесь очень довольны.")
+        return context
 
-def about(request):
-    context = {
-        'title': 'Home - О нас',
-        'content': "О нас",
-        'text_on_page': "В нашем магазине всегда самые свежие и красивые цветы. "
-                        "Вы всегда можете подобрать в нашем магазине любые цветы, букеты, корзины цветов"
-                        " на любой вкус. Мы очень стараемся, что бы удовлетворить Ваши желания. "
-                        "Приходите в наш магазин и Вы останетесь очень довольны."
-    }
+# ПРЕДЫДУЩАЯ ВЕРСИЯ:
 
-    return render(request, 'main/about.html', context)
+# def index(request):
+#
+#     context = {
+#         'title': 'Home - Главная',
+#         'content':"Магазин цветов FLOWERS",
+#     }
+#
+#     return render(request, 'main/index.html', context)
+
+# def about(request):
+#     context = {
+#         'title': 'Home - О нас',
+#         'content': "О нас",
+#         'text_on_page': "В нашем магазине всегда самые свежие и красивые цветы. "
+#                         "Вы всегда можете подобрать в нашем магазине любые цветы, букеты, корзины цветов"
+#                         " на любой вкус. Мы очень стараемся, что бы удовлетворить Ваши желания. "
+#                         "Приходите в наш магазин и Вы останетесь очень довольны."
+#     }
+#
+#     return render(request, 'main/about.html', context)
